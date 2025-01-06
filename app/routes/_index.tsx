5f79550ecdadc6e8569a4a9abf4efc85967c651b
NaufalFaisal707 <naufalfaisal707@gmail.com>
2025-01-06 16:33:04 +0700

    menambahkan halaman baru

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -223,6 +223,173 @@ export default function Index() {
           </div>
         </div>
       </section>
+
+      <section className="mt-24">
+        <div className="mx-auto flex max-w-screen-xl flex-col gap-8">
+          <h1 className="text-center text-4xl font-semibold">
+            Make your story ideas come to life with animated videos
+          </h1>
+          <h3 className="text-center text-2xl">
+            with us get more profits for your business
+          </h3>
+
+          <div className="grid grid-cols-2">
+            <img
+              src="/story-ideas.jpg"
+              alt="story ideas"
+              className="my-auto overflow-hidden rounded-3xl"
+            />
+            <div className="flex flex-col gap-4 text-start">
+              <h1 className="text-xl font-semibold">01 Marketing</h1>
+              <p>
+                Improve your audience engagement, Increases brand awareness,
+                Boosts sales conversions, Simplifies product/service
+                understanding, Enhances website traffic, Builds customer trust.
+              </p>
+
+              <h1 className="text-xl font-semibold">02 Communication</h1>
+              <p>
+                Strengthen your business brand awareness, Simplifies complex
+                concepts, Enhances product/service understanding, Reduces
+                customer doubts, Improves communication effectiveness. Clearly
+                conveys messages.
+              </p>
+
+              <h1 className="text-xl font-semibold">03 Production</h1>
+              <p>
+                High-quality, unique, and distinctive animation video production
+                based on your brand identity.
+              </p>
+
+              <Button className="w-fit" size="lg">
+                Try Now
+              </Button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* How Setara Works */}
+      <section className="mt-24">
+        <div className="mx-auto flex max-w-screen-xl flex-col gap-8">
+          <h1 className="text-center text-4xl font-semibold">
+            How Setara Studio Works?
+          </h1>
+
+          <div className="mx-auto grid max-w-4xl grid-cols-3 gap-4">
+            <div className="flex flex-col justify-between rounded-xl border bg-[#FCFCF7] p-4 text-start">
+              <img
+                src="/strategic-consulting.png"
+                alt="strategic consulting"
+                className="mx-auto my-auto w-40"
+              />
+              <p>
+                Consultation: involves collaborative analysis, concept
+                development and content creation to achieve marketing goals
+                efficiently.
+              </p>
+            </div>
+
+            <div className="flex flex-col justify-between rounded-xl border bg-[#FCFCF7] p-4 text-start">
+              <img
+                src="/thinking-pana.png"
+                alt="storytelling"
+                className="mx-auto my-auto w-40"
+              />
+              <p>
+                Storytelling: Develop compelling narratives that engage and
+                captivate the audience
+              </p>
+            </div>
+
+            <div className="flex flex-col justify-between rounded-xl border bg-[#FCFCF7] p-4 text-start">
+              <img
+                src="/rb_6056.png"
+                alt="visual stunning"
+                className="mx-auto my-auto w-40"
+              />
+              <p>
+                Create visually stunning animations with attention to detail,
+                color, composition, and aesthetics.
+              </p>
+            </div>
+
+            <div className="flex flex-col justify-between rounded-xl border bg-[#FCFCF7] p-4 text-start">
+              <img
+                src="/map-pana.png"
+                alt="innovation and creativity"
+                className="mx-auto my-auto w-40"
+              />
+              <p>
+                Innovation and Creativity: Push boundaries and explore new
+                techniques, styles, and approaches to animation.
+              </p>
+            </div>
+
+            <div className="flex flex-col justify-between rounded-xl border bg-[#FCFCF7] p-4 text-start">
+              <img
+                src="/team-pana.png"
+                alt="collaboration"
+                className="mx-auto my-auto w-40"
+              />
+              <p>
+                Collaboration: Foster effective teamwork and communication among
+                animators, designers, and other team members involved in the
+                animation process.
+              </p>
+            </div>
+
+            <div className="flex flex-col justify-between rounded-xl border bg-[#FCFCF7] p-4 text-start">
+              <img
+                src="/rb_7301.png"
+                alt="client satisfaction"
+                className="mx-auto my-auto w-40"
+              />
+              <p>
+                Client Satisfaction: Ensure that the animation meets or exceeds
+                client expectations and requirements
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-24 bg-[#FDDFBB]">
+        <div className="mx-auto flex max-w-screen-xl flex-col gap-8 p-4">
+          <h1 className="text-center text-4xl font-semibold">
+            One Team, One Goal: Producing the Best Explainer Video for You
+          </h1>
+          <h3 className="text-center text-2xl">
+            We&apos;re not just animators, but visual storytellers too!
+          </h3>
+
+          <div className="flex flex-col gap-4">
+            <h1 className="text-xl font-semibold">About Us</h1>
+            <p>
+              We offer high-quality services that focus on storytelling and a
+              deep understanding of clients&apos; needs in increasing business
+              conversions with explainer videos. Utilizing technology and
+              creative approach, we are ready to be your trusted partner in
+              achieving success in the digital world. Our services include 2D/3D
+              animated videos, motion graphics, and visual effects designed for
+              advertisements, presentations, and other digital content, ensuring
+              that each project excels and leaves a lasting impression.
+            </p>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <h1 className="text-xl font-semibold">
+              Brilliant and Creative Team
+            </h1>
+            <div className="grid grid-cols-4 *:rounded-2xl">
+              <img src="/Team.jpg" alt="team-1" />
+              <img src="/Team-1.jpg" alt="team-2" />
+              <img src="/Team-2.jpg" alt="team-3" />
+              <img src="/Team-3.jpg" alt="team-4" />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
