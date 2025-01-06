22cc8edda41467be1210a050ce7414cb3f151bff
NaufalFaisal707 <naufalfaisal707@gmail.com>
2025-01-06 09:13:54 +0700

    initialisasi shadcn/ui

--- app/root.tsx
+++ app/root.tsx
@@ -24,7 +24,7 @@ export const links: LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="id">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -45,5 +45,5 @@ export default function App() {
 }
 
 export function HydrateFallback() {
-  return <p>Loading...</p>;
+  return <p>Memuat Halaman...</p>;
 }
